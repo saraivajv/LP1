26b5ee0261da23c6824f781c6afde7a3cfa03431
Joao Victor <joaovictors2010@gmail.com>
2023-04-10 18:52:47 -0300

    Salvando questao 1

---
+++ Funcionario/Funcionario.cpp
@@ -0,0 +1,15 @@
+#include"Funcionario.hpp"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+    Funcionario::Funcionario(string n, double s, string d)
+{
+    nome = n ;
+    salario = s ;
+    departamento = d;
+}
+
+
+
---
+++ Funcionario/func.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "Funcionario.hpp"
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
